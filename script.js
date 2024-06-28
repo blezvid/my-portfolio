e3fc91e4cdecd71f09c72b8f76966d160d8a19eb
Blezel Vidal <161657239+blezvid@users.noreply.github.com>
2024-06-28 20:11:55 +0100

    html and css completed, added js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-document.getElementById('navbar-toggler').addEventListener('click', function () {
-    var navCollapse = document.getElementById('navbarSupportedContent');
-    if (navCollapse.classList.contains('show')) {
-        navCollapse.classList.remove('show');
-    } else {
-        navCollapse.classList.add('show');
-    }
-});
+
+console.log('hello')
+
+
+
+var form = document.getElementById('form')
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var fullName = document.getElementById('exampleFormControlFullname').value
+    console.log(fullName)
+    var emailAddress = document.getElementById('exampleFormControlInput1').value
+    console.log(emailAddress)
+    var visitorMessage = document.getElementById('exampleFormControlTextarea1').value
+    console.log(visitorMessage)
+})
+
